363015b3d15186c254b535f857e28df5cbf284ba
Super user <Super user@TeamCity>
2023-09-13 04:52:12 +0000

    TeamCity change in 'Apuracao2022' project: runners of 'compile' build configuration were updated

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -76,12 +76,6 @@ object Apuracao2022_Compile : BuildType({
 
     steps {
         nodeJS {
-            name = "npm ci"
-            workingDir = "apuracao-pleito-angular"
-            shellScript = "npm ci"
-            dockerImagePlatform = NodeJSBuildStep.ImagePlatform.Any
-        }
-        nodeJS {
             name = "npm run test"
             workingDir = "apuracao-pleito-angular"
             shellScript = "npm run test"
